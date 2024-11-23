4ffcf1bd6156d290c014dde0edb6295b2eed349b
AmirAli <vaziramir13771998@gmail.com>
2024-11-23 11:56:44 +0330

    edit in index.vue for update project in vercel

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@ import PronunciationApp from "~/components/PronunciationApp";
 
 export default {
   //edit Data in Pronunciation App
+  // adding data for update project
   components: {
     PronunciationApp,
   },
