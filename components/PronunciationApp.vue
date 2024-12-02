01ea0aaa604e69be9591d57e7e546d7448cf0e88
AmirAli <vaziramir13771998@gmail.com>
2024-12-02 13:54:13 +0330

    edit opening view Details

--- components/PronunciationApp.vue
+++ components/PronunciationApp.vue
@@ -179,6 +179,22 @@ export default {
               <img src="../static/yelloPlay.webp" alt="Not Found" class="absolute inset-0" />
               <div class="absolute inset-0 mb-1 text-2xl text-center mt-3 text-white font-Kiddosy">Play</div>
             </div>
+
+            <!-- Open Popup-->
+            <div v-if="showPopup" class="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center bg-black bg-opacity-50" @click="closePopup">
+              <div class="bg-white p-5 rounded-xl text-center" @click.stop>
+                <p class="font-Kiddosy text-2xl text-blue-400">{{ popupData.word.text }}</p>
+                <img :src="popupData.word.image" alt="Word Image" />
+                <p class="text-gray-600">Correct Pronunciation : <span class="font-Kiddosy text-gray-600 text-xl">{{ popupData.word.text }}</span> </p>
+                <p class="text-gray-600">Your Voice :<span class="font-Kiddosy text-gray-600 text-xl"> {{ popupData.recordedText }}</span> </p>
+                <div @click="closePopup" class="relative w-[130px] h-[61px] mx-auto cursor-pointer">
+                  <img src="../static/greenButtonIcon.webp" alt="Not Found" class="w-full h-full  object-cover absolute inset-0"/>
+                  <div class="absolute inset-0 mb-1 text-2xl text-center mt-3 text-white font-Kiddosy">
+                    Close
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
