d59a3fbcb1d4e1f15553dc099afe78c88db3a33d
amirali <vaziriamir13771998@gmail.com>
2025-05-11 00:57:02 -0700

    try to set recorded text

--- components/ResultModal.vue
+++ components/ResultModal.vue
@@ -1,7 +1,6 @@
 <template>
   <div  class="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center bg-black bg-opacity-50" @click.self="$emit('close')">
     <div class="bg-white p-5 rounded-xl text-center" @click.stop>
-      <p class="font-Kiddosy text-2xl text-blue-400">{{ word.text }}</p>
       <img :src="word.image" alt="Word Image" />
       <p class="text-gray-600">Correct Pronunciation : <span class="font-Kiddosy text-gray-600 text-xl">{{ word.text }}</span> </p>
 
