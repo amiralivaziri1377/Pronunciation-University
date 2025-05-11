50927545fd1eac42616a8d50a6f002a8d12ee56b
amirali <vaziriamir13771998@gmail.com>
2025-05-10 23:15:50 -0700

    set log

--- components/MainPronunciation.vue
+++ components/MainPronunciation.vue
@@ -188,9 +188,10 @@ const popupData = ref({ word: {}, recordedText: '' })
 
 /* HANDLERS -------------------------------------------------------------- */
 function openPopup (index) {
+  alert("hi")
   popupData.value.word         = words.value[index]
   popupData.value.recordedText = recordedText.value[index]
-  alert(popupData.value.recordedText)
+
   showPopup.value = true
 }
 const closePopup = () => (showPopup.value = false)
