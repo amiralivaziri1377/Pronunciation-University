5e3397ad6c4717eb3457b78cd7c890baa3cff9eb
Amir Ali <vaziriamirali@gmail.com>
2025-07-24 12:56:54 +0330

    edit GiveNameUserModal.vue card ui

--- components/GiveNameUserModal.vue
+++ components/GiveNameUserModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center bg-black bg-opacity-50" @click.self="$emit('close')">
+  <div class="fixed top-0 bottom-0 left-0 right-0 flex  my-2 mx-6 justify-center items-center bg-black bg-opacity-50" @click.self="$emit('close')">
     <div class="bg-white p-5 rounded-xl text-center w-full max-w-md mx-auto" @click.stop>
 
       <!-- Editable User Name Input -->
@@ -15,20 +15,11 @@
 
       <!-- Save Button -->
       <button
-        @click="$emit('save',username)"
+        @click="$emit('save',username) && $emit('close')"
         class="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-6 rounded-lg transition duration-300"
       >
         Save
       </button>
-
-      <!-- Close Button -->
-      <div @click="$emit('close')" class="relative w-[130px] h-[61px] mx-auto mt-5 cursor-pointer">
-        <img src="../static/greenButtonIcon.webp" alt="Not Found" class="w-full h-full object-cover absolute inset-0" />
-        <div class="absolute inset-0 mb-1 text-2xl text-center mt-3 text-white font-Kiddosy">
-          Close
-        </div>
-      </div>
-
     </div>
   </div>
 </template>
